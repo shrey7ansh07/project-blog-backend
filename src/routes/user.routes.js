ee4418197bb023e44bd7374f1c5bc04907d9e3ec
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-16 01:01:03 +0530

    introduced routes for login and logout

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,19 +1,14 @@
 import { Router } from "express";
-import {registerUser} from "../controllers/user.controller.js"
+import {registerUser, loginUser,logOutUser} from "../controllers/user.controller.js"
 import {upload} from "../middlewares/imageMW.js"
+import {verifyUser} from "../middlewares/authMW.js"
 
 const router = Router()
 
 router.route("/register").post(
-    upload.single({
-        name: "coverimage",
-        maxCount: 1,
-    }),
+    upload.fields([{name: "coverimage"}]),
     registerUser)
-
-
-//* here if the request is via post then we will know that we have to move to the register route
-
-
+router.route("/login").post(loginUser)
+router.route("/logout").post(verifyUser,logOutUser)
 
 export default router
