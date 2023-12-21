edf1b78dfc37297a6b92ebd02865812a9b9f7d56
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-21 20:22:30 +0530

    created error handling middleware

---
+++ src/middlewares/errorHandlerFn.js
@@ -0,0 +1,14 @@
+import {ErrorDealer} from "../utilities/errorHandler.js"
+function errorHandlerFn(err, req, res, next) {
+    if (err instanceof ErrorDealer) {
+      res.status(err.statusCode).json({
+        success: err.success,
+        message: err.message,
+        errors: err.errors,
+      });
+    } else {
+      res.status(500).json({ message: 'An unexpected error occurred' });
+    }
+  }
+
+export default errorHandlerFn
