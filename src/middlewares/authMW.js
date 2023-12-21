fbb75452fc33bdc4a65c6cf6446fa3826c1bb294
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-21 20:22:01 +0530

    modified status code

--- src/middlewares/authMW.js
+++ src/middlewares/authMW.js
@@ -6,7 +6,7 @@ const verifyUser = asyncHandler(async (req,res,next) => {
 
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
-        if(!token) {throw new ErrorDealer(404, "Unauthorized request")}
+        if(!token) {throw new ErrorDealer(401, "Unauthorized request")}
         const decodedToken = await jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
         const user = await User.findById(decodedToken?._id).select("-password -refreshtoken")
         if(!user) {throw new ErrorDealer(401,"Invalid access token")}
