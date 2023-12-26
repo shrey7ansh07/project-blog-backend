dcfc65e4d58c6de130a563cb50e5fd7248173018
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-26 17:36:41 +0530

    controller routes for authenication and update

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,20 @@
 import { Router } from "express";
-import {registerUser, loginUser,logOutUser,refreshAccessToken} from "../controllers/user.controller.js"
+import {registerUser, loginUser,logOutUser,refreshAccessToken,updateUser,updatePassword, getUser, uploadImage} from "../controllers/user.controller.js"
 import {upload} from "../middlewares/imageMW.js"
 import {verifyUser} from "../middlewares/authMW.js"
 
 const router = Router()
 
-router.route("/register").post(upload.fields([{name: "coverimage"}]),registerUser)
+router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
-router.route("/logout").post(verifyUser,logOutUser)
+
+//* secured routes that go via authentication of middlewares
+router.route("/uploadimage").post(verifyUser,upload.single('coverimage'),uploadImage)
 router.route("/refreshtoken").post(refreshAccessToken)
+router.route("/logout").post(verifyUser,logOutUser)
+router.route("/update").post(verifyUser,updateUser)
+router.route("/changepassword").post(verifyUser,updatePassword)
+router.route("/checkauth").post(verifyUser,getUser)
+
 
 export default router
