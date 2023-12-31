c3eaf773ed6733f49d06369f0b349222a0c814d0
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-31 17:05:01 +0530

    removed multiple array attributes

--- src/models/User.models.js
+++ src/models/User.models.js
@@ -2,118 +2,99 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken"
 
-const UserSchema =  mongoose.Schema(
-{
-    username : {
-        type: String,
-        required: true,
-        lowercase: true,
-        unique: true,
-        index: true,
-        trim : true
-    },
-    email : {
-        type: String,
-        required: true,
-        unique: true,
-        trim : true,
-        lowercase: true
-    },
-    fullname : {
-        type: String,
-        required: true,
-        lowercase: true,
-        trim: true
-    },
-    coverimage : {
-        type: String, //* url from cloudinary
-    },
-    quote : {
-        type: String
-    },
-    bio : {
-        type: String
-    },
-    links: [{
+const UserSchema = mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            lowercase: true,
+            unique: true,
+            index: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true
+        },
+        fullname: {
+            type: String,
+            required: true,
+            lowercase: true,
+            trim: true
+        },
+        coverimage: {
+            type: String, //* url from cloudinary
+        },
+        quote: {
             type: String
-    }],
-    quote: {
-        type: String
-    },
-    followers: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-    }],
-    following: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-    }],
-    blogsread : [{
-            type : mongoose.Schema.Types.ObjectId,
-            ref : "Blog" 
+        },
+        bio: {
+            type: String
+        },
+        links: [{
+            type: String
         }],
-    blogsliked : [{
-            type : mongoose.Schema.Types.ObjectId,
-            ref : "Blog"
-    }],
-    blogsonread : [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Blog"
-    }],
+        followers: {
+            type: Number,
+            default: 0,
+        },
+        following: {
+            type: Number,
+            default: 0,
+        },
 
-    //* these consist of the id's of the blogs he read
-    password : {
-        type: String, //* because we will decrypt the password before storing it 
-        required: [true, "password is required"],
-       
+        //* these consist of the id's of the blogs he read
+        password: {
+            type: String, //* because we will decrypt the password before storing it 
+            required: [true, "password is required"],
+
+        },
+
+        refreshToken: {
+            type: String,
+        }
     },
-    
-    refreshToken: {
-        type: String,
+    {
+        timestamps: true
     }
-},
-{
-    timestamps: true
-}
 )
 
-UserSchema.pre("save", async function(next)
-{
-    if(!this.isModified("password")) return next();
-    try{
-        this.password = await bcrypt.hash(this.password,10);
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    try {
+        this.password = await bcrypt.hash(this.password, 10);
         next();
     }
-    catch (error){
+    catch (error) {
         next(error)
     }
 })
 
-UserSchema.methods.isPasswordCorrect = async function(password)
-{
-    return await bcrypt.compare(password,this.password)
+UserSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
 }
 
-UserSchema.methods.generateAccessToken = function()
-{
-return jwt.sign({
+UserSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
         _id: this._id,
         email: this.email,
         username: this.username,
         fullname: this.fullname
     },
         process.env.ACCESS_TOKEN_SECRET,
-       { expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
     )
 }
-UserSchema.methods.generateRefreshToken = function()
-{
-return jwt.sign(
-    {
-        _id: this._id,
-    },
+UserSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
         process.env.REFRESH_TOKEN_SECRET,
-       { expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
     )
 }
 
