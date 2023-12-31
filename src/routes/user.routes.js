5051ec14b2c0a37a6689841faad9f599846ec269
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-31 17:05:29 +0530

    indentation and added routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,19 @@
 import { Router } from "express";
-import {registerUser, loginUser,logOutUser,refreshAccessToken,updateUser,updatePassword, getUser, uploadImage} from "../controllers/user.controller.js"
-import {upload} from "../middlewares/imageMW.js"
-import {verifyUser} from "../middlewares/authMW.js"
+import {
+    registerUser,
+    loginUser,
+    logOutUser,
+    refreshAccessToken,
+    updateUser,
+    updatePassword,
+    getUser,
+    uploadImage,
+    report,
+    followUser,
+    isfollowing
+} from "../controllers/user.controller.js"
+import { upload } from "../middlewares/imageMW.js"
+import { verifyUser } from "../middlewares/authMW.js"
 
 const router = Router()
 
@@ -9,12 +21,15 @@ router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
 
 //* secured routes that go via authentication of middlewares
-router.route("/uploadimage").post(verifyUser,upload.single('coverimage'),uploadImage)
+router.route("/uploadimage").post(verifyUser, upload.single('coverimage'), uploadImage)
 router.route("/refreshtoken").post(refreshAccessToken)
-router.route("/logout").post(verifyUser,logOutUser)
-router.route("/update").post(verifyUser,updateUser)
-router.route("/changepassword").post(verifyUser,updatePassword)
-router.route("/checkauth").post(verifyUser,getUser)
+router.route("/logout").post(verifyUser, logOutUser)
+router.route("/update").post(verifyUser, updateUser)
+router.route("/changepassword").post(verifyUser, updatePassword)
+router.route("/checkauth").post(verifyUser, getUser)
+router.route("/report").post(verifyUser, report)
+router.route("/follow/:userId").post(verifyUser, followUser)
+router.route("/isfollowing/:userId").get(verifyUser, isfollowing)
 
 
 export default router
