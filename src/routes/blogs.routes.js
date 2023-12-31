1bdb54ed5f774b40d943ea41f381fb6c2b924b4c
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-31 17:05:12 +0530

    added routes

--- src/routes/blogs.routes.js
+++ src/routes/blogs.routes.js
@@ -1,13 +1,22 @@
 import { Router } from "express";
 import { verifyUser } from "../middlewares/authMW.js";
-import {postBlog,getBlog, editBlog,deleteBlog} from "../controllers/blog.controller.js"
+import {
+    postBlog,
+    getBlog,
+    editBlog,
+    deleteBlog,
+    updateBlogStatus,
+    getPopular
+} from "../controllers/blog.controller.js"
 
 const router = Router()
 
 //* protected routes
-router.route("/addblog").post(verifyUser,postBlog)
-router.route("/editblog/:userId").patch(verifyUser,editBlog,getBlog)
-router.route("/getblogs/:userId").get(verifyUser,getBlog)
-router.route("/deleteblog/:blogId").post(verifyUser,deleteBlog)
+router.route("/addblog").post(verifyUser, postBlog)
+router.route("/editblog/:userId").patch(verifyUser, editBlog, getBlog)
+router.route("/getblogs/:userId").get(verifyUser, getBlog)
+router.route("/deleteblog/:blogId").post(verifyUser, deleteBlog)
+router.route("/updatestatus/:blogId").patch(verifyUser, updateBlogStatus)
+router.route("/getpopular").get(verifyUser, getPopular)
 
 export default router
